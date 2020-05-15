52f2548a3677c34a3a0374889f3cc1afbfe28534
Honeyfield <HoneyfieK@corning.com>
2020-05-14 21:34:00 -0400

    fetchData

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('pants')
+
+const rando = 'https://randomuser.me/api/?results=12'
+
+function fetchData(url){
+    return fetch(url)
+             .then(response => response.json())
+             .catch(err => console.log(err))
+   }
